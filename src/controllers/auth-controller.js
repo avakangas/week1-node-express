4dd1a8feb3eb2b30a26c05eac2cbe1c38e06ae78
avakangas <ava.kangas@metropolia.fi>
2025-02-27 16:41:29 +0200

    done

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,17 +1,36 @@
-import { selectUserByNameAndPassword } from "../models/user-model";
+
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import 'dotenv/config';
+import {selectUserByUsername} from '../models/user-model.js';
+import {customError} from '../middlewares/error-handler.js';
 
 // user authentication (login)
-const login = async (req, res) => {
+const login = async (req, res, next) => {
   const {username, password} = req.body;
   if (!username) {
-    return res.status(401).json({message: 'Username missing.'});
+    return next(customError('Username missing.', 400));
+    //return res.status(401).json({message: 'Username missing.'});
   }
-  const user = await selectUserByNameAndPassword(username, password);
+  const user = await selectUserByUsername(username);
+  // jos käyttäjä löytyi tietokannasta verrataan kirjautumiseen syötettyä sanaa tietokannan
+  // salasanatiivisteeseen
   if (user) {
-    res.json({message: 'login ok', user});
-  } else {
-    res.status(401).json({message: 'Bad username/password.'});
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      const token = jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+      });
+      return res.json({message: 'login ok', user, token});
+    }
   }
+  //res.status(401).json({message: 'Bad username/password.'});
+  next(customError('Bad username/password.', 401));
 };
 
-export {login};
+const getMe = (req, res) => {
+  const user = req.user;
+  res.json(user);
+};
+
+export {login, getMe};
